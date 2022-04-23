6347d870a2dc46940f7a6b2bd0137b2290a9e449
LevOlegovich <lev-glo90@yandex.ru>
2022-04-23 22:02:34 +0400

    Исправлена ошибка.Доработка

--- src/lesson15/TestTinkoff.java
+++ src/lesson15/TestTinkoff.java
@@ -2,49 +2,72 @@ package lesson15;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 
+
 public class TestTinkoff {
+
+//    По вершинам правильного 2019-угольника ходит представитель Тинькофф.
+//    Вершины 2019-угольника пронумерованы по порядку.
+//    За один шаг он переходит сразу на +15углов (Т.е. 1,16,31ит.д.), и так по кругу.
+//    Как только представитель попадает в ту точку,где он уже был,он останавливается.
+//    Свой поход представитель начинает в углу под номером 1.
+//    Сколько углов он обойдет?
+
     public static void main(String[] args) {
 
-        Collection<Long> b = new HashSet<>();
-        ArrayList c = new ArrayList();
+        ArrayList<Long> b = new ArrayList<>();
+
 
         for (long k = 1; k <= 2019; k = k + 15) {
             b.add(k);
-
         }
 
+        int step = 15; // шаг
+        long countSteps = b.size() - 1; // количество сделанных шагов (посещенных углов)
+        long countRepeat = 1; // прошли первый круг
+        long i = b.get(b.size() - 1) + step; //или = 7; // начало следующего круга
+        boolean checkPoint = false;
 
-        long corner = 1;
-        long i = 7;
-        boolean chek = true;
-
-
-        System.out.println(b.size());
+        System.out.println(b);//
+        System.out.println();
+        System.out.println("Последний угол в первом круге. Номер: " + b.get(b.size() - 1));
 
 
         do {
 
-            if (i > 2019) {
+            if (i > 2019) { // начало следующего круга. Начинается со второго
                 i = i - 2019;
             }
-            corner = corner + 1;
-            for (i = i; i <= 2019; i = i + 15) {
+            countRepeat = countRepeat + 1; // круг 2 и т.д
+
+
+            for (; i <= 2019; i = i + step) {
+
+                countSteps = countSteps + 1;
                 if (b.contains(i)) {
-                    System.out.println("успех " + i);
-                    chek = false;
+                    System.out.print("Ура! Представитель компании вернулся в исходный угол " + i);
+                    System.out.println();
+                    checkPoint = true;
                 }
-                if (chek == false) {
-                    System.out.println(corner);
-                    System.out.println(b.size() * corner);
+
+
+                if (checkPoint == true) {
+
+                    System.out.println("Это произошло на " + countRepeat + " круге");
+                    System.out.println();
+                    System.out.println("Отает:" +
+                            "Количесвто  шагов (количество посещенных РАЗНЫХ углов) = " + countSteps);
+                    System.out.println();
                     return;
+
                 }
 
             }
 
 
-        } while (true);
+        } while (countRepeat < 5000);// если что условие поменять на true, в зависимости от задачи.
 
 
     }
